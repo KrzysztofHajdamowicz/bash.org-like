d78f26047ebfba083dc9ab1c5af786f3c27f56d5
Krzysztof Hajdamowicz <KrzysztofHajdamowicz@users.noreply.github.com>
2017-02-07 19:02:39 +0100

    pep8 compatibility

--- quotes/models.py
+++ quotes/models.py
@@ -1,21 +1,25 @@
 from django.db import models
 from django.utils import timezone
 
+STATUS_CHOICES = (
+    (1, 'Is pending'),
+    (2, 'Is rejected'),
+    (3, 'is approved'),
+)
+
+
 # Create your models here.
 class Quote(models.Model):
-	acceptant = models.ForeignKey('auth.User', null=True)
-	content = models.TextField(null=False)
-	created_date = models.DateTimeField(default=timezone.now)
-	votes_up = models.PositiveIntegerField(default=0)
-	votes_down = models.PositiveIntegerField(default=0)
-	# status
-	# 1 - is pending
-	# 2 - is rejected
-	# 3 - is approved
-	status = models.PositiveIntegerField(default=1)
+    acceptant = models.ForeignKey('auth.User', null=True)
+    content = models.TextField(null=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    votes_up = models.PositiveIntegerField(default=0)
+    votes_down = models.PositiveIntegerField(default=0)
+    status = models.PositiveIntegerField(default=1, choices=STATUS_CHOICES)
 
-	def publish(self):
-		self.published_date = timezone.now()
-		self.save()
-	def __str__(self):
-		return self.content
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.content
